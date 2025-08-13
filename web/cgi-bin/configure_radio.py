f814bf0264691cadfed05a856b95e260c32d41cb
Daniel Mirsky <danielmirsky@microsoft.com>
2025-08-13 09:21:03 -0700

    fix web scripts and add radio control

--- web/cgi-bin/configure_radio.py
+++ web/cgi-bin/configure_radio.py
@@ -2,6 +2,24 @@
 import cgi, cgitb
 import sys
 import subprocess
+import traceback
+
+def hz_to_inc(hz):
+    return int( hz * (2**27)/(125e6) )
+
+def run(command):
+    print(f"<br>Running: {command}")
+    try:
+        res = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    except:
+        print("<br><pre>")
+        traceback.print_exc()
+        print("</pre>")
+        sys.exit(1)
+    print(f"<br><pre>{res.stdout}</pre>")
+
+# Redirect stderr so we can see it in browser
+sys.stderr = sys.stdout
 
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
@@ -10,7 +28,7 @@ form = cgi.FieldStorage()
 adc_freq_hz  = int(form.getvalue('adc_freq_hz'))
 tune_freq_hz  = int(form.getvalue('tune_freq_hz'))
 streaming = form.getvalue('streaming')
-
+streaming_enabled_int = int(streaming == "streaming")
 
 # Send the result to the browser
 print ("Content-type:text/html")
@@ -27,6 +45,24 @@ if (streaming == "streaming"):
     print ("streaming is Enabled<br>")
 else :
     print ("streaming is Disabled<br>")
+
+# adc
+p=f"devmem 0x43c00000 w {hz_to_inc(adc_freq_hz)}"
+run(p)
+
+# tuner
+p=f"devmem 0x43c00004 w {hz_to_inc(tune_freq_hz)}"
+run(p)
+
+# control (bit 0 is nreset, bit 4 is streaming enable)
+p=f"devmem 0x43c00008 w 0x{streaming_enabled_int}0"
+run(p)
+
+#Get counter
+print("getting counter for sanity check")
+p="devmem 0x43c0000c w"
+run(p)
+
 print ('</body>')
 print ('</html>')
 
--- web/cgi-bin/setup_all.sh
+++ web/cgi-bin/setup_all.sh
@@ -2,12 +2,27 @@
 echo "Content-type: text/html" # Tells the browser what kind of content to expect
 echo "" # An empty line. Mandatory, if it is missed the page content will not load
 echo "<p><em>"
+
+# can't get configure_codec.sh to stay executable on my dev system for some reason
+echo "setting configure_codec.sh executable<br>"
+chmod a+x configure_codec.sh
+
+echo "compiling udp_radio<br>"
+echo "<pre>" && gcc udp_radio.c -o udp_radio 2>&1 && echo "</pre><br>"
+echo "compiling test_radio<br>"
+echo "<pre>" && gcc test_radio.c -o test_radio 2>&1 && echo "</pre><br>"
+echo "compiling udpsender<br>"
+echo "<pre>" && gcc udpsender.c -o udpsender 2>&1 && echo "</pre><br>"
+echo "<br>"
+
 echo "loading PL...<br>"
 fpgautil -b design_1_wrapper.bit.bin
 echo "</p></em><p>"
 echo "configuring Codec...<br>"
 ./configure_codec.sh
 echo "</p>"
-echo "starting my UDP Streamer Program Here...<br>"
-./udp_radio 192.168.1.3
+echo "Starting my UDP Streamer Program Here...<br>"
+./udp_radio 192.168.1.3 > ../udp_radio_log &
+echo "log <a href='/udp_radio_log'>here</a><br>"
+echo "Started UDP Streamer<br>"
 echo "<p><em>All Done!</em></p>" 
